cf457041182af17ccdfa04ebe878e02d67c7d653
Shivam Bansal <shivam2032.cse18@chitkara.edu.in>
2021-05-17 18:06:02 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
+import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense';
 
-import Expenses from './components/Expenses';
-
-function App() {
+const App=()=> {
   const expenses = [
     {
       id: 'e1',
@@ -24,10 +24,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
